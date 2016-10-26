d1977ed43293e21a5bdee60123bd98463218fb0c
AdamRakaska <arakaska@gmail.com>
2016-10-25 19:35:23 -0600

    Update to BigDecimal

--- SemiprimeVisualizer/BigIntegerExtensionMethods.cs
+++ SemiprimeVisualizer/BigIntegerExtensionMethods.cs
@@ -8,34 +8,59 @@ using System.Collections.Generic;
 namespace SemiprimeVisualizer
 {
 	/// <summary>
-	/// Aquired from http://mjs5.com/2016/01/20/c-biginteger-helper-constructors
+	/// Sqrt and NRoot acquired from http://mjs5.com/2016/01/20/c-biginteger-helper-constructors
 	/// </summary>
 	public static class BigIntegerExtensionMethods
 	{
-		/// <summary>
-		/// Returns the square root of a BigInteger number
-		/// </summary>
-		/// <param name="number">The number.</param>
-		/// <returns></returns>
-		public static BigInteger Sqrt(this BigInteger number)
+		private static BigInteger Fifteen = new BigInteger(15);
+		public static bool IsSquare(this BigInteger input)
+		{
+			if (input == BigInteger.Zero) { return false; }
+
+			int bitwiseAnd = (int)(input & Fifteen);
+
+			if (bitwiseAnd > 9)
+			{
+				return false; // return immediately in 6 cases out of 16.
+			}
+
+			//if (bitwiseAnd == 0 || bitwiseAnd == 1 || bitwiseAnd == 4 || bitwiseAnd == 9)  // squares in base 16 end in 0, 1, 4, or 9
+			if (bitwiseAnd != 2 && bitwiseAnd != 3 && bitwiseAnd != 5 && bitwiseAnd != 6 && bitwiseAnd != 7 && bitwiseAnd != 8)
+			{
+				BigInteger sqrt = input.Sqrt();
+				BigInteger sqr = sqrt.Square();
+				bool dividedEvenly = (sqr == input);
+				return dividedEvenly;   // return (input.Sqrt().Square() == input);
+			}
+			return false;
+
+		}
+
+		public static BigInteger Square(this BigInteger input)
+		{
+			return input * input;
+		}
+
+		// Returns the square root of a BigInteger number
+		public static BigInteger Sqrt(this BigInteger input)
 		{
 			BigInteger n = 0, p = 0;
-			if (number == BigInteger.Zero)
+			if (input == BigInteger.Zero)
 			{
 				return BigInteger.Zero;
 			}
-			var high = number >> 1;
+			var high = input >> 1;
 			var low = BigInteger.Zero;
 
 			while (high > low + 1)
 			{
 				n = (high + low) >> 1;
 				p = n * n;
-				if (number < p)
+				if (input < p)
 				{
 					high = n;
 				}
-				else if (number > p)
+				else if (input > p)
 				{
 					low = n;
 				}
@@ -44,21 +69,25 @@ namespace SemiprimeVisualizer
 					break;
 				}
 			}
-			return number == p ? n : low;
+			return input == p ? n : low;
 		}
 
-		/// <summary>
-		/// Returns the NTHs root of a BigInteger with Remainder
-		/// </summary>
-		/// <param name="value">The value.</param>
-		/// <param name="root">The root.</param>
-		/// <param name="remainder">The remainder.</param>
-		/// <returns></returns>
-		/// <exception cref="System.Exception">
-		/// root must be greater than or equal to 1
-		/// or
-		/// value must be a positive integer
-		/// </exception>
+		public static BigInteger Sqrt2(BigInteger num)
+		{
+			if (num == BigInteger.Zero) { return 0; }   // Avoid zero divide  
+
+			BigInteger n = (num / 2) + 1; // Initial estimate, never low  
+			BigInteger n1 = (n + (num / n)) / 2;
+			while (n1 < n)
+			{
+				n = n1;
+				n1 = (n + (num / n)) / 2;
+			}
+			return n;
+		}
+
+		// Returns the NTHs root of a BigInteger with Remainder.
+		// The root must be greater than or equal to 1 or value must be a positive integer.
 		public static BigInteger NthRoot(this BigInteger value, int root, ref BigInteger remainder)
 		{
 			if (root < 1)
